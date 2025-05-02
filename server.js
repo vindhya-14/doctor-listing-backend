cf2224c529315753a33fc86d30527cb98dd05263
Vindhya-14 <vindhya3321@gmail.com>
2025-05-02 16:19:36 +0530

    added url

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.json());
 app.use("/api/doctors", doctorRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    console.log(" MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(` Server running on port ${process.env.PORT || 5000}`);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(" DB connection error:", err));
